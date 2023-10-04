76b899c15228e45866deba783deee23a3f6a32f8
Abdallah <bedohossam535@gmail.com>
2023-10-04 05:09:01 +0300

    deploy

---
+++ src/components/TodoForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import shortid from "shortid";
+
+function TodoForm(props) {
+  const [text, setText] = useState("");
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.random(),
+      text: text,
+      complete: false,
+    });
+    setText("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        className="input-field"
+        type="text"
+        onChange={handleChange}
+        value={text}
+      />
+      <button className="btn" onClick={handleSubmit}>
+        Add Todo
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
